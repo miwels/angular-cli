08a4d4664ffa8176246d7df1fe669d202a3dea25
Miguel Martin <miguel.martin@thefoundry.co.uk>
2016-11-24 12:35:46 +0000

    removed some files

--- src/app/test/1st.spec.js
+++
@@ -1,8 +0,0 @@
-/// <reference path="../../../typings/browser/ambient/jasmine/index.d.ts" />
-describe('1st tests', function () {
-    it('true is true', function () { return expect(true).toBe(true); });
-});
-describe('2nd test', function () {
-    it('false is false', function () { return expect(false).toBe(false); });
-});
-//# sourceMappingURL=1st.spec.js.map
